38c74e316413b409c10c815a720a27a1f53de2c1
Cristerean <efratyuda57@gmail.com>
2026-01-22 12:25:07 +0430

    Add files via upload

---
+++ Pertemuan1KomMath.m
@@ -0,0 +1,8 @@
+% "Menghitung Luas Lingkaran"
+
+clc
+clear
+
+r = 7 ;
+L = pi*r^2 ;
+disp (L)
---
+++ Pertemuan2KomMath.m
@@ -0,0 +1,73 @@
+% if...and
+% n = input ('nilai :  ') ;
+% if n >= 70 
+%     disp ('anda lulus') ;
+% end 
+% disp ('coba lagi') ;
+
+
+% if...else...and
+% n = input ('nilai :  ') ;
+% if n > 70 
+%     disp ('anda lulus') ;
+% else 
+%     disp ('cobalagi') ; 
+% end
+
+% if...elseif...elseif...and
+% n = input ('Tinggi :') ;
+% if n > 190
+%     disp ('Sangat tinggi') ;
+% elseif n > 160 && n < 190  
+%     disp ('Cukup tinggi') ;
+% elseif n <= 160 && n > 100
+%     disp ('Pendek') ;
+% else 
+%     disp ('Mini') ;
+% end
+
+% Switch
+
+
+
+% LOOP
+
+% for_1
+% for i = 1:10
+%     disp (i)
+% end
+
+
+
+% for_2
+% a =0;
+% for i = 1:5
+%     a = a+i;
+% end
+% disp (a)
+
+
+
+% Continue
+% a =0;
+% for i = 1:5
+%     if i == 2
+%         continue 
+%     else 
+%         a = a+i;
+%     end
+% end
+% disp (a)
+
+% break
+for i = 1:10
+    if i == 5
+        break
+    else 
+        disp (i)
+    end
+end
+
+
+
+
